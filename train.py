249d56657d87f140ce93f2a56c6d9bfeb9eedbd5
Rickyyy-zh <zhangq_07@outlook.com>
2022-01-25 22:03:06 +0800

    update

---
+++ train.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2
+import torch
+import torchvision
+import torch.nn as nn
+import os
+
+from torchvision import transforms
+
+def train():
+    pass
+
+
+if __name__ == "__main__":
+    train()
