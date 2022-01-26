fd336c774a0e0f6432e2a660d2ec1dca13fb17b3
Rickyyy-zh <zhangq_07@outlook.com>
2022-01-26 21:51:24 +0800

    up

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # UCAS CV Assignment 2 煤矸石分割任务
 
-github rep: rickyyy-zh/cv_assignment2
+github rep: rickyyy-zh/cv_assignment2 via ssh
 
 ## *Abstract：*
 
--- train.py
+++ train.py
@@ -4,12 +4,12 @@ import torch
 import torchvision
 import torch.nn as nn
 import os
-import utils
+from utils import get_labels
 
 from torchvision import transforms
 
 def train():
-    
+    get_labels("./test/")
     pass
 
 
